74c3c0396a53eee9d29dda0ba87ff17e5d0bbd93
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-10-06 14:08:40 -0400

    Basic routes.

--- vote/www.py
+++ vote/www.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask import render_template, request
 from tropo import Tropo
+import json
 
 app = Flask(__name__)
 
@@ -12,9 +13,22 @@ def hello():
     tropo.say("Hello, World")
     return tropo.RenderJson()
 
+@app.route('/state0.json', methods=['GET'])
+def state_0():
+    tropo = Tropo()
+    tropo.ask(choices="[ANY]", say="Ahoy-hoy! Tell us, what state do you live in?", timeout=60)
+    return tropo.RenderJson()
+
 @app.route('/', methods=['GET'])
 def homepage():
-    return "<html><body>Can I vote? Text <code>can i vote</code> to 2274</body></html>"
+    return """
+<html>
+    <body style="color:#fdf6e3;background-color:#002b36;font-size:64px;font-family:Helvetica,'Helvetica',sans-serif;margin:20px auto;width:1024px;display:block;text-align:center;">
+        <h1 style="font-size:128px;">Can I vote?</h1>
+        <p>Text <code style="color:#d33682;font-weight:bold;">vote</code> to 2274.</p>
+    </body>
+</html>
+"""
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
