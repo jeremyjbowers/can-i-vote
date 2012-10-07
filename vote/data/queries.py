cb32e105534eb9706172414ba2395a01ace8fbce
Serdar Tumgoren <zstumgoren@gmail.com>
2012-10-07 14:15:44 -0400

    more db queries + doctests and a data update

--- vote/data/queries.py
+++ vote/data/queries.py
@@ -6,6 +6,7 @@ USAGE
 
 >>> from vote.data import api
 >>> api.has_voting_deadline_passed('TN')
+False
 
 """
 import datetime
@@ -42,8 +43,8 @@ class QueryAPI(object):
     def get_voting_deadline(self, state):
         """
         >>> from vote.data import api
-        >>> api.has_voting_deadline_passed('CA')
-        True
+        >>> api.get_voting_deadline('CA')
+        datetime.date(2012, 10, 22)
 
         """
         tbl = self.meta.tables['voting_cal']
@@ -57,10 +58,46 @@ class QueryAPI(object):
     def elec_agency_phone(self, state):
         """
         >>> from vote.data import api
-        >>> api.elec_agency_phone('CA')
+        >>> api.elec_agency_phone('TX')
+        u'(512) 463-5650'
+
         """
         tbl = self.meta.tables['voting_cal']
         query = select([tbl.c.sos_phone], tbl.c.state_postal == state)
+        return self.engine.execute(query).fetchone()[0]
+
+    def is_id_required(self, state):
+        """
+        >>> from vote.data import api
+        >>> api.is_id_required('TX')
+        False
+
+        """
+        tbl = self.meta.tables['id_required']
+        query = select([tbl.c.voter_id_state], tbl.c.state_postal == state)
+        return bool(self.engine.execute(query).fetchone()[0])
+
+    def primary_form_of_id(self, state):
+        """
+        >>> from vote.data import api
+        >>> api.primary_form_of_id('TX')
+        u"State-issued driver's license"
+
+        """
+        tbl = self.meta.tables['rules']
+        query = self.engine.execute(select([tbl.c.voter_identification_id, tbl.c.voter_identification_name], tbl.c.state == state))
+        data = sorted([(int(row[0]), row[1]) for row in query.fetchall()])
+        return data[0][1]
+
+    def other_acceptable_ids(self, state):
+        """
+        >>> from vote.data import api
+        >>> api.other_acceptable_ids('TX')[:13]
+        u'Existing law:'
+
+        """
+        tbl = self.meta.tables['rules_if_no_id']
+        query = select([tbl.c.acceptable_forms_of_id], tbl.c.state_postal == state)
         return self.engine.execute(query).fetchone()[0]
 
 api = QueryAPI()
