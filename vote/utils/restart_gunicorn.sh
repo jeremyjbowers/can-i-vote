c6eb470484fa6c6a5f5e4116685e2148bd5009f9
Jeremy Bowers <jeremyjbowers@gmail.com>
2012-10-06 21:07:25 -0400

    More fab.

--- vote/utils/restart_gunicorn.sh
+++ vote/utils/restart_gunicorn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source virtualenvwrapper.sh
 export PATH=/root/.virtualenvs/canivote/bin:$PATH
-export PYTHONPATH=/root/.virtualenvs/canivote/lib:/home/canivote/can-i-vote/vote
+export PYTHONPATH=/root/.virtualenvs/canivote/lib/python2.7/site-packages:/home/canivote/can-i-vote/vote
 killall gunicorn
 /root/.virtualenvs/canivote/bin/gunicorn -w 4 www:app -D --log-file /tmp/gunicorn.log -b 127.0.0.1:8000
